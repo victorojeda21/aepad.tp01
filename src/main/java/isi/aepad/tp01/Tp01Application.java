1446f58ab39bfcfbaf9ccedb89efac28fb9a22e6
Victor Ojeda <victorojeda21@gmail.com>
2019-05-09 16:26:42 -0300

    Subida del proyecto

---
+++ src/main/java/isi/aepad/tp01/Tp01Application.java
@@ -0,0 +1,13 @@
+package isi.aepad.tp01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp01Application.class, args);
+	}
+
+}
---
+++ src/test/java/isi/aepad/tp01/Tp01ApplicationTests.java
@@ -0,0 +1,16 @@
+package isi.aepad.tp01;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Tp01ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
